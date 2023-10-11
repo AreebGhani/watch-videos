5f006e2aa799753db8341cd25f125b71bd9424de
AreebGhani <areebghanidps@gmail.com>
2023-10-11 14:43:04 -0700

    Make Responsive

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+    $(".search-field").hide();
+    $(".search-icon").click(function (event) {
+        event.preventDefault();
+        $(".search-field").toggleClass("d-flex");
+        event.stopPropagation();
+    });
+    $(document).click(function (event) {
+        if (
+            !$(event.target).closest(".search-field").length &&
+            !$(event.target).closest(".search-icon").length
+        ) {
+            $(".search-field").hide();
+        }
+    });
+    $("#sidebar-toggle").click(function () {
+        $("#sidebar").toggleClass("show");
+        $("#sidebar-toggle i").toggleClass("fa-times");
+    });
     $(window).scroll(function () {
         if ($(this).scrollTop() > 20) {
             $('#backToTopBtn').fadeIn();
